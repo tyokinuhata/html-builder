6e01121321fcf248e2feac9456caf7bd668d634a
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-13 02:31:21 +0900

    リファクタリング

--- HtmlBuilder.php
+++ HtmlBuilder.php
@@ -11,14 +11,24 @@ class HtmlBuilder
         $this->pointer = 0;
     }
 
+    /**
+     * 配列に対して要素を挿入します
+     * @param $beginElement
+     * @param null $endElement
+     * @return $this
+     */
     private function arrayElementInsert($beginElement, $endElement = null)
     {
-        array_splice($this->element, $this->pointer, 0, '<' . $beginElement . '>');
-        $this->pointer++;
+        array_splice($this->element, $this->pointer++, 0, '<' . $beginElement . '>');
         array_splice($this->element, $this->pointer, 0, '</' . (is_null($endElement) ? $beginElement : $endElement) . '>');
         return $this;
     }
 
+    /**
+     * 配列に対してテキストを挿入します
+     * @param $text
+     * @return $this
+     */
     private function arrayTextNodeInsert($text)
     {
         array_splice($this->element, $this->pointer, 0, $text);
@@ -26,48 +36,80 @@ class HtmlBuilder
         return $this;
     }
 
-    public function out()
-    {
-        $this->pointer++;
-        return $this;
-    }
-
+    /**
+     * テキストを受け取り, 渡します
+     * @param $text
+     * @return $this
+     */
     public function text($text)
     {
         $this->arrayTextNodeInsert($text);
         return $this;
     }
 
+    /**
+     * html要素を渡します
+     * @return $this
+     */
     public function html()
     {
         $this->arrayElementInsert('html');
         return $this;
     }
 
+    /**
+     * head要素を渡します
+     * @return $this
+     */
     public function head()
     {
         $this->arrayElementInsert('head');
         return $this;
     }
 
+    /**
+     * body要素を渡します
+     * @return $this
+     */
     public function body()
     {
         $this->arrayElementInsert('body');
         return $this;
     }
 
+    /**
+     * title要素を渡します
+     * @return $this
+     */
     public function title()
     {
         $this->arrayElementInsert('title');
         return $this;
     }
 
+    /**
+     * h1要素を渡します
+     * @return $this
+     */
     public function h1()
     {
         $this->arrayElementInsert('h1');
         return $this;
     }
 
+    /**
+     * ポインタを１階層上へ戻します
+     * @return $this
+     */
+    public function out()
+    {
+        $this->pointer++;
+        return $this;
+    }
+
+    /**
+     * 出力します
+     */
     public function append()
     {
         var_dump($this->element);
