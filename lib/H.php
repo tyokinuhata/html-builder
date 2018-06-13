d0236e146bc769ab1f8d4910379faab8112c7d22
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-13 10:11:00 +0900

    h2 ~ h6の実装

--- lib/H.php
+++ lib/H.php
@@ -13,4 +13,54 @@ trait H
         $this->arrayElementInsert('h1');
         return $this;
     }
+
+    /**
+     * h2要素を渡します
+     * @return $this
+     */
+    public function h2()
+    {
+        $this->arrayElementInsert('h2');
+        return $this;
+    }
+
+    /**
+     * h3要素を渡します
+     * @return $this
+     */
+    public function h3()
+    {
+        $this->arrayElementInsert('h3');
+        return $this;
+    }
+
+    /**
+     * h4要素を渡します
+     * @return $this
+     */
+    public function h4()
+    {
+        $this->arrayElementInsert('h4');
+        return $this;
+    }
+
+    /**
+     * h5要素を渡します
+     * @return $this
+     */
+    public function h5()
+    {
+        $this->arrayElementInsert('h5');
+        return $this;
+    }
+
+    /**
+     * h6要素を渡します
+     * @return $this
+     */
+    public function h6()
+    {
+        $this->arrayElementInsert('h6');
+        return $this;
+    }
 }
