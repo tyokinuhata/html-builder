bff2050e04a7a06eafe13fc0d34d3dc642c2cce5
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-14 00:37:03 +0900

    Update Text

--- lib/Text.php
+++ lib/Text.php
@@ -5,12 +5,99 @@ namespace HtmlBuilder;
 trait Text
 {
     /**
+     * a要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function a(...$options)
+    {
+        $this->arrayElementsInsert('a', ...$options);
+        return $this;
+    }
+
+    /**
+     * strong要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function strong(...$options)
+    {
+        $this->arrayElementsInsert('strong', ...$options);
+        return $this;
+    }
+
+    /**
+     * sub要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function sub(...$options)
+    {
+        $this->arrayElementsInsert('sub', ...$options);
+        return $this;
+    }
+
+    /**
+     * sup要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function sup(...$options)
+    {
+        $this->arrayElementsInsert('sup', ...$options);
+        return $this;
+    }
+
+    /**
+     * i要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function i(...$options)
+    {
+        $this->arrayElementsInsert('i', ...$options);
+        return $this;
+    }
+
+    /**
+     * b要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function b(...$options)
+    {
+        $this->arrayElementsInsert('b', ...$options);
+        return $this;
+    }
+
+    /**
+     * span要素を渡します
+     * @param array ...$options
+     * @return $this
+     */
+    public function span(...$options)
+    {
+        $this->arrayElementsInsert('span', ...$options);
+        return $this;
+    }
+
+    /**
      * br要素を渡します
      * @return $this
      */
     public function br()
     {
         $this->arrayElementInsert('br');
+        return $this;
+    }
+
+    /**
+     * wbr要素を渡します
+     * @return $this
+     */
+    public function wbr()
+    {
+        $this->arrayElementInsert('wbr');
         return $this;
     }
 
