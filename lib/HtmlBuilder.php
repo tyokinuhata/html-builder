2078e86235545b7cf251733a391a02e0fa50ab46
Kazuki Tobita <kazukiti201@gmail.com>
2018-06-18 18:45:33 +0900

    リファクタリング

--- lib/HtmlBuilder.php
+++ lib/HtmlBuilder.php
@@ -13,55 +13,8 @@ class HtmlBuilder
         $this->pointer = 1;
     }
 
-    /**
-     * ポインタをn階層上へ戻します
-     * @param int $depth
-     * @return $this
-     */
-    public function upstream($depth = 1)
-    {
-        $this->pointer += $depth;
-        return $this;
-    }
-
-    /**
-     * dev: 開発者モード
-     * prod: プロダクトモード
-     * min: プロダクトモード(ミニファイ)
-     * で出力します
-     * @param $mode
-     */
-    public function append($mode = 'min')
-    {
-        if ($mode === 'dev')
-        {
-            var_dump($this->buffer);
-        }
-        else if ($mode === 'prod')
-        {
-            // TODO: 実装
-        }
-        else if ($mode === 'min')
-        {
-            echo implode('', $this->buffer);
-        }
-    }
-
-    /**
-     * 任意のパスに任意の名前のhtmlファイルを生成します
-     * @param $path
-     */
-    public function build($path)
-    {
-        $fileName = $path . '.html';
-        if (!file_exists($fileName))
-        {
-            touch($fileName);
-        }
-        file_put_contents($fileName, $this->buffer);
-    }
-
     use Insert;
+    use Core;
     use Html\Root;
     use Html\Meta;
     use Html\Script;
